9b1839cea2c84879747bb2062ac43a6d5a0b089a
zummashu <samarasdelta@gmail.com>
2021-08-25 15:45:11 +0300

    project page works with id

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -2,11 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Project = (props) => {
-  return <h1>{`Project ID: ${props.id}`}</h1>;
+  return (
+    <div>
+      <h1>{`Project Title: ${props.project.projectTitle}`}</h1>
+      <div>Project Category: {`${props.project.projectCategory}`}</div>
+    </div>
+  );
 };
 
 export default Project;
 
 Project.propTypes = {
-  id: PropTypes.node,
+  id: PropTypes.string,
+  project: PropTypes.shape({
+    projectTitle: PropTypes.string,
+    projectCategory: PropTypes.string,
+  }),
 };
--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Project from "../components/project/Project.js";
 import { useParams } from "react-router";
+import PropTypes from "prop-types";
 
-function ProjectPage() {
+function ProjectPage(props) {
   let { id } = useParams();
+  const [project, setProject] = useState({});
+
+  useEffect(() => {
+    fetch(`/api/projects/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setProject(data);
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  });
 
   return (
     <div>
-      <Project id={id} />
+      <Project id={id} project={project} />
     </div>
   );
 }
 
 export default ProjectPage;
+
+ProjectPage.propTypes = {
+  id: PropTypes.node,
+};
