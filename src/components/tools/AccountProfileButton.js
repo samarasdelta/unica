04113def5507736fa507cc542fce0d626d2b6cf0
zummashu <samarasdelta@gmail.com>
2021-12-15 16:32:06 +0200

    profile modal change

--- src/components/tools/AccountProfileButton.js
+++ src/components/tools/AccountProfileButton.js
@@ -6,7 +6,6 @@ import {
   Drawer,
   Divider,
   Box,
-  ListSubheader,
   Grid,
   List,
   ListItem,
@@ -16,11 +15,7 @@ import PersonIcon from "@material-ui/icons/Person";
 import { Link as RouteLink } from "react-router-dom";
 import SettingsIcon from "@material-ui/icons/Settings";
 import ExitToAppSharpIcon from "@material-ui/icons/ExitToAppSharp";
-// import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-// import { CustomThemeContext } from "../themes/CustomThemeProvider";
-// import SwitchUI from "@material-ui/core/Switch";
-// import "./MoreButtonCSS.css";
 
 const useStyles = makeStyles({
   list: {
@@ -33,37 +28,11 @@ const useStyles = makeStyles({
   },
 });
 
-// const CustomSwitch = withStyles({
-//   switchBase: {
-//     color: "#5C91FF",
-//     "&$checked": {
-//       color: "#5C91FF",
-//     },
-//     "&$checked + $track": {
-//       backgroundColor: "#5C91FF",
-//     },
-//   },
-//   checked: {},
-//   track: {},
-// })(SwitchUI);
-
 export default function TemporaryDrawer() {
   const classes = useStyles();
   const [state, setState] = React.useState({
     right: false,
   });
-
-  // const { currentTheme, setTheme } = useContext();
-  // const isDark = Boolean(currentTheme === "dark");
-
-  // const handleThemeChange = (event) => {
-  //   const { checked } = event.target;
-  //   if (checked) {
-  //     setTheme("dark");
-  //   } else {
-  //     setTheme("normal");
-  //   }
-  // };
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (
@@ -88,7 +57,6 @@ export default function TemporaryDrawer() {
       <Box mx={2}>
         <Divider />
         <div>
-          <ListSubheader align="center">ece00792@uowm.gr</ListSubheader>
           <Divider />
           <List className={classes.listContent}>
             <ListItem>
