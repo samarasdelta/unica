40ee142e3101bf888e46925b0903a3409b5db955
zummashu <samarasdelta@gmail.com>
2021-12-15 16:21:32 +0200

    discover table changes

--- src/components/discover/DiscoverListItem.js
+++ src/components/discover/DiscoverListItem.js
@@ -8,9 +8,7 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
-import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
-import Moment from "react-moment";
 
 const columns = [
   { id: "title", label: "Title" },
@@ -20,34 +18,6 @@ const columns = [
     label: "Owner",
     align: "right",
   },
-  {
-    id: "lmodified",
-    label: "Last Modified",
-    align: "right",
-  },
-];
-
-function createData(title, category, owner, size) {
-  const lmodified = owner / size;
-  return { title, category, owner, size, lmodified };
-}
-
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  // createData("China", "CN", 1403500365, 9596961),
-  // createData("Italy", "IT", 60483973, 301340),
-  // createData("United States", "US", 327167434, 9833520),
-  // createData("Canada", "CA", 37602103, 9984670),
-  // createData("Australia", "AU", 25475400, 7692024),
-  // createData("Germany", "DE", 83019200, 357578),
-  // createData("Ireland", "IE", 4857000, 70273),
-  // createData("Mexico", "MX", 126577691, 1972550),
-  // createData("Japan", "JP", 126317000, 377973),
-  // createData("France", "FR", 67022000, 640679),
-  // createData("United Kingdom", "GB", 67545757, 242495),
-  // createData("Russia", "RU", 146793744, 17098246),
-  // createData("Nigeria", "NG", 200962417, 923768),
-  // createData("Brazil", "BR", 210147125, 8515767),
 ];
 
 const useStyles = makeStyles({
@@ -55,7 +25,7 @@ const useStyles = makeStyles({
     width: "100%",
   },
   container: {
-    maxHeight: 440,
+    maxHeight: "auto",
   },
 });
 
@@ -87,16 +57,6 @@ const DataTable = () => {
   // }, []);
 
   const classes = useStyles();
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-  };
 
   return (
     <Paper className={classes.root}>
@@ -139,30 +99,12 @@ const DataTable = () => {
                       </Typography>
                     </Grid>
                   </TableCell>
-                  <TableCell>
-                    <Grid container direction="column" alignItems="flex-end">
-                      <Typography color="textPrimary" variant="body1">
-                        <Moment position="right" format="DD/MM/YYYY - HH:mm:ss">
-                          {project.dateCreated}
-                        </Moment>
-                      </Typography>
-                    </Grid>
-                  </TableCell>
                 </TableRow>
               </TableBody>
             );
           })}
         </Table>
       </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 20]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
     </Paper>
   );
 };
