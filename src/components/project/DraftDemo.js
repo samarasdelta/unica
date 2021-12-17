f40c68a1371c63808797ed0065b2838b9726bdd9
zummashu <samarasdelta@gmail.com>
2021-12-17 20:19:38 +0200

    editor converts to html

--- src/components/project/DraftDemo.js
+++ src/components/project/DraftDemo.js
@@ -1,24 +1,75 @@
+// import React, { useState } from "react";
+// import { ContentState, convertToRaw } from "draft-js";
+// import { Editor } from "react-draft-wysiwyg";
+// import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+// import "./democss.css";
+
+// const DemoEditor = () => {
+//   let _contentState = ContentState.createFromText("Write something!");
+//   const raw = convertToRaw(_contentState);
+//   const [contentState, setContentState] = useState(raw); // ContentState JSON
+
+//   return (
+//     <div className="DemoEditor">
+//       <Editor
+//         defaultContentState={contentState}
+//         onContentStateChange={setContentState}
+//         wrapperClassName="wrapper-class"
+//         editorClassName="editor-class"
+//         toolbarClassName="toolbar-class"
+//       />
+//     </div>
+//   );
+// };
+// export default DemoEditor;
+
+//end
+
 import React, { useState } from "react";
-import { ContentState, convertToRaw } from "draft-js";
+import { EditorState } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
+import { convertToHTML } from "draft-convert";
+import DOMPurify from "dompurify";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "./democss.css";
 
 const DemoEditor = () => {
-  let _contentState = ContentState.createFromText("Write something!");
-  const raw = convertToRaw(_contentState);
-  const [contentState, setContentState] = useState(raw); // ContentState JSON
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+  const [convertedContent, setConvertedContent] = useState(null);
+
+  const handleEditorChange = (state) => {
+    setEditorState(state);
+    convertContentToHTML();
+  };
+
+  const convertContentToHTML = () => {
+    let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(currentContentAsHTML);
+  };
+
+  const createMarkup = (html) => {
+    return {
+      __html: DOMPurify.sanitize(html),
+    };
+  };
 
   return (
     <div className="DemoEditor">
       <Editor
-        defaultContentState={contentState}
-        onContentStateChange={setContentState}
+        editorState={editorState}
+        onEditorStateChange={handleEditorChange}
         wrapperClassName="wrapper-class"
         editorClassName="editor-class"
         toolbarClassName="toolbar-class"
       />
+      <div
+        className="preview"
+        dangerouslySetInnerHTML={createMarkup(convertedContent)}
+      ></div>
     </div>
   );
 };
+
 export default DemoEditor;
