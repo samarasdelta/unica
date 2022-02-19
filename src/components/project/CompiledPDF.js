c11848d566ded91a5771e5b9d9b229123df99612
zummashu <samarasdelta@gmail.com>
2022-02-19 20:04:57 +0200

    safety commit

--- src/components/project/CompiledPDF.js
+++ src/components/project/CompiledPDF.js
@@ -5,11 +5,11 @@ const CompiledPdf = () => {
     <object
       width="100%"
       height="100%"
-      data="http://localhost:4000/api/latex/public/fda8de62-be91-4601-8f74-75a372633a38.pdf"
+      data="http://localhost:4000/api/latex/public/48b25b2e-59af-42a7-afba-c3cbaded8d96.pdf"
       type="application/pdf"
     >
       <embed
-        src="http://localhost:4000/api/latex/public/fda8de62-be91-4601-8f74-75a372633a38.pdf"
+        src="http://localhost:4000/api/latex/public/48b25b2e-59af-42a7-afba-c3cbaded8d96.pdf"
         type="application/pdf"
       />
     </object>
