2c251a14324e39df76b92bd61047d2dc14d324f8
zummashu <samarasdelta@gmail.com>
2022-03-06 14:13:40 +0200

    adding comments

--- src/components/project/CompiledLatex.js
+++ src/components/project/CompiledLatex.js
@@ -12,6 +12,15 @@ const CompiledLatex = ({ url }) => {
         >
           {"Click the compile button to preview your work as a PDF."}
         </Typography>
+        <Typography
+          variant="h6"
+          color="textSecondary"
+          style={{ padding: "1vw" }}
+        >
+          {
+            "After compile is done, you can share this pdf by clicking the Share PDF button. "
+          }
+        </Typography>
       </div>
       <div className="pdf-viewer">
         <object width="100%" height="100%" data={url} type="application/pdf">
