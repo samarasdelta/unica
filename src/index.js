5c8a0aae1f2d17a6f2edf7de3feeccace6b1110a
zummashu <samarasdelta@gmail.com>
2022-06-11 13:24:40 +0300

    trying to fix the font bug

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import CustomThemeProvider from "../src/components/tools/themes/CustomThemeProvider";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import "typeface-poppins";
 // import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
