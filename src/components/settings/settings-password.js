6156b6019de44f48b012ab72f3c4eb03bb866b36
zummashu <samarasdelta@gmail.com>
2022-07-05 20:57:44 +0300

    fixing relative paths 4

--- src/components/settings/settings-password.js
+++ src/components/settings/settings-password.js
@@ -18,7 +18,7 @@ export const SettingsPassword = () => {
   const [passConfirm, setPassConfirm] = React.useState("");
 
   const updateUser = async () => {
-    await fetch(`/api/users/me`, {
+    await fetch(`${process.env.REACT_APP_API_URL}/api/users/me`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
